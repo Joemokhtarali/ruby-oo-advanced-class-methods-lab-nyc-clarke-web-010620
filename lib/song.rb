b931f39b2332263cd3e2f4d9ff01b40cbf7d2ac9
Mokhtar Ali <joemokhtarali@gmail.com>
2020-01-13 14:50:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,15 @@ class Song
     self.class.all << self
   end
 
+  # def self.create(name)
+  #   song = self.new 
+  #   song.name = name
+  #   @@all << song 
+  # end 
+  
+  def self.create(name)
+    song = self.new
+    song.name = name
+    @@all << song
+  end
 end
